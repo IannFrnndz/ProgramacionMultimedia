0d6189e366a4057d330cee28817df1627bc49473
Ian Fernández Gamo <ian.fernandezgamo24@campusfp.es>
2025-10-09 10:43:33 +0200

    Calculadora android studio

---
+++ Calculadora_Ian/app/src/main/java/com/example/calculadora_ian/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.calculadora_ian;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i("LifeCycle","onPause");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.i("LifeCycle","onPause");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i("LifeCycle","onPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i("LifeCycle","onPause");
+    }
+
+    @Override
+    protected void onPostResume(){
+        super.onPostResume();
+    }
+}
