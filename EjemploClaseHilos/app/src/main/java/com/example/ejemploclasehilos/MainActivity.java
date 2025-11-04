4536eb769defde6a39df8ce5fa5564b686e8a573
Ian Fernández Gamo <ian.fernandezgamo24@campusfp.es>
2025-11-04 11:12:54 +0100

    ejercicio de hilos con alphas

--- EjemploClaseHilos/app/src/main/java/com/example/ejemploclasehilos/MainActivity.java
+++ EjemploClaseHilos/app/src/main/java/com/example/ejemploclasehilos/MainActivity.java
@@ -1,27 +1,34 @@
 package com.example.ejemploclasehilos;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.jetbrains.annotations.Async;
+
 public class MainActivity extends AppCompatActivity {
 
+    private Button variableQueGuardaUnBoton;
+    Handler handler = new Handler(Looper.getMainLooper());
 
-    private Button variable_que_guarada_un_boton;
+    TextView viewContador ;
 
-    private TextView textito;
-    private final Handler handler_guapo = new Handler(Looper.getMainLooper());
+    ImageView imagealfa;
 
+    int  concurrencia = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,42 +40,145 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        textito = findViewById(R.id.textView);
-        variable_que_guarada_un_boton = findViewById(R.id.buttonGuapardo);
+        variableQueGuardaUnBoton = findViewById(R.id.buttonGuapardo);
+        viewContador = findViewById(R.id.textView_contador);
+        imagealfa = findViewById(R.id.imageViewAlfa);
+        variableQueGuardaUnBoton.setOnClickListener(new View.OnClickListener() {
 
-        variable_que_guarada_un_boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new MiHiloPausa().start();
+                //new HiloJ().start();
+                //new HiloContador().start();
+                //AsyncTask a = new MiClaseAsincrona().execute();
+                int cpu_cores = Runtime.getRuntime().availableProcessors();
+
+                if(cpu_cores>concurrencia){
+                    imagealfa.setImageAlpha(0);
+
+                    AsyncTask a = new MiClaseAsincronaAlfa().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                    concurrencia++;
+                }
+
             }
         });
     }
 
-    class MiHiloPausa extends Thread
-    {
+    private class MiClaseAsincrona extends AsyncTask<String, Integer, String>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            viewContador.setText("Comienza la cuenta atras");
+        }
 
         @Override
-        public void run() {
-            super.run();
-
-            for(int i = 0; i <= 100; i++)
-            {
-                int value = i;
-                handler_guapo.post(()-> textito.setText("Contador : " + value));
-                try{
+        protected String doInBackground(String... strings) {
+            int i = 0;
+            while(i<=20){
+                publishProgress(i);
+                try {
                     Thread.sleep(1000);
-                    /*new Handler(Looper.getMainLooper()).post(()->
-                            Toast.makeText(MainActivity.this, "TERMINÉ EL SLEEEP", Toast.LENGTH_LONG).show()
-                    );*/
+                    i++;
+                } catch ( InterruptedException e) {
+                    return e.getLocalizedMessage();
+                }
+            }
+            return "Se acabo la cuenta atras";
+        }
 
-                }catch (InterruptedException e){
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            if(viewContador!=null){
+                viewContador.setText("contador " + values[0] + " el valor x1000 es: " + values[0] * 1000);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            viewContador.setText(s);
+            concurrencia--;
+
+        }
+    }
+    private class MiClaseAsincronaAlfa extends AsyncTask<String, Integer, String>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            viewContador.setText("Empieza a cambiar");
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            int i = 0;
+
+            while(i<=1000){
+                publishProgress(i);
+                try {
+                    Thread.sleep(500);
+                    i++;
+                } catch ( InterruptedException e) {
+                    return e.getLocalizedMessage();
+                }
+
+            }
+            return "Se acabo la cuenta atras";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            if(viewContador!=null && imagealfa != null){
+                imagealfa.setImageAlpha(values[0]);
+                viewContador.setText("El alfa es de:  " + values[0]);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            viewContador.setText(s);
+            concurrencia--;
+        }
+    }
+    class HiloJ extends Thread {
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(10000);
+
+                new Handler(Looper.getMainLooper()).post(() ->
+                        Toast.makeText(MainActivity.this, "TERMINE EL SLEEP", Toast.LENGTH_SHORT).show()
+                );
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    class HiloContador extends Thread {
+        @Override
+        public void run() {
+
+
+
+            for (int i = 0; i <= 1500; i++) {
+
+                int finalI = i; // necesario porque i se usa dentro del Runnable
+
+                handler.post(() -> {
+                    viewContador.setText(String.valueOf(finalI));
+                });
+
+                try {
+                    Thread.sleep(1000); // retardo para ver el conteo (10ms)
+                } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-
-
         }
-
-
     }
+
+
 }
